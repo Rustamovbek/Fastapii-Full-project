a1a799af4e86a93e427764eb9cf1c0c45d5ab343
multicode <mr.mirmakhmudov16112008@gmail.com>
2025-04-29 22:31:35 +0500

    fix some issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,17 +7,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from fastapi.testclient import TestClient
 import uuid
-
 from main import app
 from core.database import Base
-from main import get_db
+from core.db_conf import get_db
 from models.user import User
 
-# Parolni hash qilish funksiyasi
 def get_password_hash(password: str) -> str:
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
-# Test uchun vaqtinchalik database
 TEST_DB_FILE = tempfile.NamedTemporaryFile(delete=False)
 TEST_SQLALCHEMY_DATABASE_URL = f"sqlite:///{TEST_DB_FILE.name}"
 
@@ -26,7 +23,7 @@ engine = create_engine(
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# DB override
+
 def override_get_db():
     db = TestingSessionLocal()
     try:
@@ -34,7 +31,6 @@ def override_get_db():
     finally:
         db.close()
 
-# Database setup/teardown
 @pytest.fixture(scope="session", autouse=True)
 def setup_and_teardown():
     Base.metadata.create_all(bind=engine)
@@ -44,12 +40,12 @@ def setup_and_teardown():
     TEST_DB_FILE.close()
     os.unlink(TEST_DB_FILE.name)
 
-# Test client
+
 @pytest.fixture
 def client():
     return TestClient(app)
 
-# Test user yaratish
+
 @pytest.fixture
 def test_user():
     db = TestingSessionLocal()
